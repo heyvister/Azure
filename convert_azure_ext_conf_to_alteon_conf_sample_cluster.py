61fb70072de64fdebe3672cec92df9e1dea4cabd
idohe <IdoHe@radware.com>
2018-11-28 08:54:16 +0200

    fdgdfrfsdfgdf

--- convert_azure_ext_conf_to_alteon_conf_sample_cluster.py
+++ convert_azure_ext_conf_to_alteon_conf_sample_cluster.py
@@ -95,7 +95,7 @@ def convert_service_to_config():
     else:
         private_ip = server_dict["PRIVATE_IP_ADDRESS_PREFIX"] + str(server_dict["PRIVATE_IP_ADDRESS_POSIX_START"]+1)
 
-    output_file.write("/c/slb/virt 1\n\tdis\n\tvip " + private_ip +"\n")
+    output_file.write("/c/slb/virt 1\n\tena\n\tvip " + private_ip +"\n")
   
     if "SLB_PORT" in server_dict:
         if len(server_dict["SLB_PORT"]) > 0:
